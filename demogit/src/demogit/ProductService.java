b2183c5a4274b03d689fba7932292caa4e5c9468
ISundev <sunnnne04@gmail.com>
2023-10-29 22:10:32 +0700

    create by sun

--- demogit/src/demogit/ProductService.java
+++ demogit/src/demogit/ProductService.java
@@ -9,5 +9,40 @@ package demogit;
  * @author maccuacu
  */
 public class ProductService {
+    private int id;
+    private String name,status;
+
+    public ProductService() {
+    }
+
+    public ProductService(int id, String name, String status) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
     
 }
--- demogit/src/demogit/Products.java
+++ demogit/src/demogit/Products.java
@@ -9,5 +9,41 @@ package demogit;
  * @author maccuacu
  */
 public class Products {
+    private int id;
+    private String name,status;
+
+    public Products() {
+    }
+
+    public Products(int id, String name, String status) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+    }
+
     
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
