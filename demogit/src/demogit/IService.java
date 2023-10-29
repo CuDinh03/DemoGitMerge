fd4a32af620516f891a9b6500c776e2865da84d7
cudh <cudh@crmviet.vn>
2023-10-29 22:17:49 +0700

    findall

--- demogit/src/demogit/IService.java
+++ demogit/src/demogit/IService.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package demogit;
+
+import java.util.List;
 
 /**
  *
  * @author maccuacu
  */
-public class IService {
-    
+public interface IService {
+    List<Products> findAll();
+       
 }
